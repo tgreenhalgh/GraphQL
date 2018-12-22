1ad4a6a6bb5e5f438b832d5f286be2b7030911ca
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-12-21 16:16:16 -0800

    adds subscription for updating posts

--- basics/src/resolvers/Mutation.js
+++ basics/src/resolvers/Mutation.js
@@ -91,14 +91,46 @@ const Mutation = {
 
     return post;
   },
-  updatePost(parent, args, { db }, info) {
+  updatePost(parent, args, { db, pubsub }, info) {
     const { id, data } = args;
     const post = db.posts.find(post => post.id === id);
+    const originalPost = { ...post };
 
     if (!post) throw new Error('Post not found');
+
     if (typeof data.title === 'string') post.title = data.title;
     if (typeof data.body === 'string') post.body = data.body;
-    if (typeof data.published === 'boolean') post.published = data.published;
+
+    if (typeof data.published === 'boolean') {
+      post.published = data.published;
+
+      // checking status of post
+      if (originalPost.published && !post.published) {
+        // deleted
+        pubsub.publish(`post`, {
+          post: {
+            mutation: 'DELETED',
+            data: originalPost,
+          },
+        });
+      } else if (!originalPost.published && post.published) {
+        // created
+        pubsub.publish(`post`, {
+          post: {
+            mutation: 'CREATED',
+            data: post,
+          },
+        });
+      }
+    } else if (post.published) {
+      // updated
+      pubsub.publish(`post`, {
+        post: {
+          mutation: 'UPDATED',
+          data: post,
+        },
+      });
+    }
 
     return post;
   },
